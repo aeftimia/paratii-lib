57c8e53299a726aed640e097ea2b328703fbca9d
Yahya <ya7yaz@gmail.com>
2017-12-21 17:05:55 +0200

    browser tests are almost ready (1 fail, 1 skipped)

--- test/browser.js
+++ test/browser.js
@@ -1,8 +1,8 @@
 // tests required here will be ran in the browser.
-// require('./paratii.ipfs.js')
+require('./paratii.ipfs.js')
 require('./paratii.eth.js')
-// require('./paratii.eth.users.js')
-// require('./paratii.eth.vids.js')
-// require('./paratii.eth.wallet.js')
-// require('./paratii.eth.personal.js')
+require('./paratii.eth.users.js')
+require('./paratii.eth.vids.js')
+require('./paratii.eth.wallet.js')
+// require('./paratii.personal.js')
 // require('./paratii.js')
